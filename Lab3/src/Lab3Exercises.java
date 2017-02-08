c0be7c24d7bc2f070d84c465db5e2bf215c23b6a
4015d19 <4015d19@mandarina.ece.uprm.edu>
2017-02-08 14:19:30 -0400

    Changed name of the JFrame box with my name, Adrian Soto, changed the size and the location of the box into the center, approximately.

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -6,6 +6,7 @@ public class Lab3Exercises {
 		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		myFrame.setLocation(900, 400);
 		myFrame.setSize(200, 200);
+		
 		myFrame.setVisible(true);
 		
 	}
